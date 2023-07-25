3351f8b73ef6fb5a75d5868cde0df46f4073d07a
Marco-willian <marcowcandido@gmail.com>
2023-07-25 11:52:13 -0300

    2M

--- script.js
+++ script.js
@@ -2,16 +2,56 @@ import   pressing   from "./keys.js";
 import  audio  from "./sounds.js";
 
 
-const pegaValor = document.querySelector("#send");
+const pegaValor1 = document.querySelector("#first_send");
+const pegaValor2 = document.querySelector("#second_send");
+const pegaValor3 = document.querySelector("#third_send");
+const pegaValor4 = document.querySelector("#fouth_send");
+const pegaValor5 = document.querySelector("#fifth_send");
 
-pegaValor.addEventListener("click", function(event){
+let firt_value;
+let second_value;
+let third_value;
+let fouth_value;
+let fifth_value;
+
+pegaValor1.addEventListener("click", function(event){
   event.preventDefault();
 
-  const name = document.querySelector("#custom_key");
+  const name = document.querySelector("#first_custom_key");
 
-  const value = name.value;
+  firt_value = name.value;
+})
 
-  console.log(value)
+pegaValor2.addEventListener("click", function(event){
+  event.preventDefault();
+
+  const name = document.querySelector("#second_custom_key");
+
+  second_value = name.value;
+})
+
+pegaValor3.addEventListener("click", function(event){
+  event.preventDefault();
+
+  const name = document.querySelector("#third_custom_key");
+
+  third_value = name.value;
+})
+
+pegaValor4.addEventListener("click", function(event){
+  event.preventDefault();
+
+  const name = document.querySelector("#fouth_custom_key");
+
+  fouth_value = name.value;
+})
+
+pegaValor5.addEventListener("click", function(event){
+  event.preventDefault();
+
+  const name = document.querySelector("#fifth_custom_key");
+
+  fifth_value = name.value;
 })
 
 window.addEventListener(
@@ -24,23 +64,23 @@ window.addEventListener(
       let soundToPlay = null;
 
       switch (event.key) {
-        case pressing.keyArrowDown:
+        case firt_value:
           soundToPlay = new Audio(audio.boom);        
           break;
-        case pressing.keyArrowUp:
+        case second_value:
           soundToPlay = new Audio(audio.clap);      
           break;
-        case pressing.KeyArrowLeft:
+        case third_value:
           soundToPlay = new Audio(audio.hihat);        
           break;
-        case pressing.KeyArrowRight:
-          soundToPlay = new Audio(audio.kick)        
+        case fouth_value:
+          soundToPlay = new Audio(audio.kick);        
           break;
-        case pressing.keyD:
+        case fifth_value:
           if(event.ctrlKey){
-            soundToPlay = new Audio(audio.tink)
+            soundToPlay = new Audio(audio.tink);
           }else{
-            soundToPlay = new Audio(audio.snare)
+            soundToPlay = new Audio(audio.snare);
           }
           break;
         default:
