5cbaf2f42eb0df66c37632d8fe291a545a253959
Marco-willian <marcowcandido@gmail.com>
2023-08-04 17:46:02 -0300

    6add and 4mod

--- controleDeCoresDinamico.js
+++ controleDeCoresDinamico.js
@@ -1,3 +1,5 @@
+import audio from "./sounds.js";
+
 var areasClique = document.querySelectorAll('.tocadores_colunas');
 var cores = ['#1ccb38', '#c1093c', '#a60dc3', '#d09c58', '#B9CF77', '#CFBF80'];
 var corPadrao = '#5A5A5A';
@@ -17,12 +19,76 @@ function mudarCor(areaClique, cor) {
   areaClique.style.backgroundColor = cor;
 }
 
-var quadrado = document.querySelectorAll('#som');
+var quadrado_re = document.querySelectorAll('.som_re');
+var quadrado_si = document.querySelectorAll('.som_si');
+var quadrado_do = document.querySelectorAll('.som_do');
+var quadrado_mi = document.querySelectorAll('.som_mi');
+var quadrado_fa = document.querySelectorAll('.som_fa');
+var quadrado_kick = document.querySelectorAll('.kick');
 
-var som = 'sounds/piano/316913__jaz_the_man_2__si.wav';
+quadrado_re.forEach( elemento => {
+  var som = audio[1].re;
+  
+  var quadrado = new Audio(som);
 
-var sons = Array.from(quadrado).map( ()=> som);
-
-quadrado.addEventListener('click', function() {
-    sons.play();
+  elemento.addEventListener('click', () => {
+    quadrado.currentTime = 0;
+    quadrado.play();
+  });
 });
+
+quadrado_si.forEach( elemento => {
+  var som = audio[1].si;
+  
+  var quadrado = new Audio(som);
+
+  elemento.addEventListener('click', () => {
+    quadrado.currentTime = 0;
+    quadrado.play();
+  });
+});
+
+quadrado_do.forEach( elemento => {
+  var som = audio[1].do;
+  
+  var quadrado = new Audio(som);
+
+  elemento.addEventListener('click', () => {
+    quadrado.currentTime = 0;
+    quadrado.play();
+  });
+});
+
+quadrado_mi.forEach( elemento => {
+  var som = audio[1].mi;
+  
+  var quadrado = new Audio(som);
+
+  elemento.addEventListener('click', () => {
+    quadrado.currentTime = 0;
+    quadrado.play();
+  });
+});
+
+quadrado_fa.forEach( elemento => {
+  var som = audio[1].fa;
+  
+  var quadrado = new Audio(som);
+
+  elemento.addEventListener('click', () => {
+    quadrado.currentTime = 0;
+    quadrado.play();
+  });
+});
+
+quadrado_kick.forEach( elemento => {
+  var som = audio[1].kick;
+  
+  var quadrado = new Audio(som);
+
+  elemento.addEventListener('click', () => {
+    quadrado.currentTime = 0;
+    quadrado.play();
+  });
+});
+
--- sounds.js
+++ sounds.js
@@ -1,15 +1,24 @@
-const audio = 
-{    
-        "boom": "sounds/drums/virtual-drum_sounds_boom.wav",
-        "clap": "sounds/drums/virtual-drum_sounds_clap.wav",
-        "hihat": "sounds/drums/virtual-drum_sounds_hihat.wav",
-        "kick": "sounds/drums/virtual-drum_sounds_kick.wav",
-        "openhat": "sounds/drums/virtual-drum_sounds_openhat.wav",
-        "ride": "sounds/drums/virtual-drum_sounds_ride.wav",
-        "snare": "sounds/drums/virtual-drum_sounds_snare.wav",
-        "tink": "sounds/drums/virtual-drum_sounds_tink.wav",
-}
+const audio =[
 
-
+        {    
+                "boom": "sounds/drums/virtual-drum_sounds_boom.wav",
+                "clap": "sounds/drums/virtual-drum_sounds_clap.wav",
+                "hihat": "sounds/drums/virtual-drum_sounds_hihat.wav",
+                "kick": "sounds/drums/virtual-drum_sounds_kick.wav",
+                "openhat": "sounds/drums/virtual-drum_sounds_openhat.wav",
+                "ride": "sounds/drums/virtual-drum_sounds_ride.wav",
+                "snare": "sounds/drums/virtual-drum_sounds_snare.wav",
+                "tink": "sounds/drums/virtual-drum_sounds_tink.wav",
+        },
+        {
+                "si" : 'sounds/piano/316913__jaz_the_man_2__si.wav',
+                "re" : 'sounds/piano/316908__jaz_the_man_2__re.wav',
+                "do" : 'sounds/piano/316900__jaz_the_man_2__do-stretched-octave.wav',
+                "mi" : 'sounds/piano/316906__jaz_the_man_2__mi.wav',
+                "fa" : 'sounds/piano/316904__jaz_the_man_2__fa.wav',
+                "kick" : 'sounds/drums/105677__soniktec__kick-fx-marker-286.wav',
+        }
+                
+] 
 
 export default audio;
