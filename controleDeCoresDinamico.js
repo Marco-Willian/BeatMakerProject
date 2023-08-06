b8994e9d0b5129d67e3c6107e8db3a81915d902d
Marco-Willian <marcowcandido@gmail.com>
2023-08-06 12:06:18 -0300

    3mod's

--- controleDeCoresDinamico.js
+++ controleDeCoresDinamico.js
@@ -5,6 +5,7 @@ var cores = ['#1ccb38', '#c1093c', '#a60dc3', '#d09c58', '#B9CF77', '#CFBF80'];
 var corPadrao = '#5A5A5A';
 
 var coresAtuais = Array.from(areasClique).map(() => corPadrao);
+var coresAtivas = new Set();
 
 areasClique.forEach(function(areaClique, index) {
   areaClique.addEventListener('click', function() {
@@ -12,6 +13,18 @@ areasClique.forEach(function(areaClique, index) {
     var novaCor = corAtual === corPadrao ? cores[index] : corPadrao;
     mudarCor(areaClique, novaCor);
     coresAtuais[index] = novaCor;
+
+    if(novaCor !== corPadrao){
+      areaClique.classList.add('ativo');
+    }else{
+      areaClique.classList.remove('ativo');
+    }
+
+    if (novaCor === corPadrao) {
+      coresAtivas.delete(index);
+    }else{
+      coresAtivas.add(index);
+    }
   });
 });
 
@@ -19,76 +32,60 @@ function mudarCor(areaClique, cor) {
   areaClique.style.backgroundColor = cor;
 }
 
-var quadrado_re = document.querySelectorAll('.som_re');
-var quadrado_si = document.querySelectorAll('.som_si');
-var quadrado_do = document.querySelectorAll('.som_do');
-var quadrado_mi = document.querySelectorAll('.som_mi');
-var quadrado_fa = document.querySelectorAll('.som_fa');
-var quadrado_kick = document.querySelectorAll('.kick');
+function reproduzirSonsEmSequencia() {
+  var indicesAtivos = Array.from(coresAtivas);
 
-quadrado_re.forEach( elemento => {
-  var som = audio[1].re;
-  
+  function reproduzirProximoSom(indice){
+     if(indice < indicesAtivos.length){
+      
+      var som = audio[1].kick;
+      var quadrado = new Audio(som);
+
+      quadrado.currentTime = 0;
+      quadrado.play();
+
+      quadrado.onended = function() {
+        reproduzirProximoSom(indice + 1);
+      }
+     }
+  }
+  reproduzirProximoSom(0);
+}
+
+var botaoPlay = document.getElementById('play');
+botaoPlay.addEventListener('click', reproduzirSonsEmSequencia);
+
+
+/*
+------ Sai som ao clicar ------
+
+const soundMap = {
+  '.som_re': 're',
+  '.som_si': 'si',
+  '.som_do': 'do',
+  '.som_mi': 'mi',
+  '.som_fa': 'fa',
+  '.kick': 'kick',
+};
+
+function adicionarEventoESom(elemento, somKey) {
+  var som = audio[1][somKey];
   var quadrado = new Audio(som);
 
   elemento.addEventListener('click', () => {
     quadrado.currentTime = 0;
     quadrado.play();
   });
-});
+}
 
-quadrado_si.forEach( elemento => {
-  var som = audio[1].si;
-  
-  var quadrado = new Audio(som);
+for (const selector in soundMap) {
+  const elements = document.querySelectorAll(selector);
+  const soundKey = soundMap[selector];
 
-  elemento.addEventListener('click', () => {
-    quadrado.currentTime = 0;
-    quadrado.play();
+  elements.forEach((elemento) => {
+    adicionarEventoESom(elemento, soundKey);
   });
-});
+}
 
-quadrado_do.forEach( elemento => {
-  var som = audio[1].do;
-  
-  var quadrado = new Audio(som);
-
-  elemento.addEventListener('click', () => {
-    quadrado.currentTime = 0;
-    quadrado.play();
-  });
-});
-
-quadrado_mi.forEach( elemento => {
-  var som = audio[1].mi;
-  
-  var quadrado = new Audio(som);
-
-  elemento.addEventListener('click', () => {
-    quadrado.currentTime = 0;
-    quadrado.play();
-  });
-});
-
-quadrado_fa.forEach( elemento => {
-  var som = audio[1].fa;
-  
-  var quadrado = new Audio(som);
-
-  elemento.addEventListener('click', () => {
-    quadrado.currentTime = 0;
-    quadrado.play();
-  });
-});
-
-quadrado_kick.forEach( elemento => {
-  var som = audio[1].kick;
-  
-  var quadrado = new Audio(som);
-
-  elemento.addEventListener('click', () => {
-    quadrado.currentTime = 0;
-    quadrado.play();
-  });
-});
+*/
 
