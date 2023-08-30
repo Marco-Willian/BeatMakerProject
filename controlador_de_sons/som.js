6a313a29a9399533b4e326692d3fdf77ce48122f
Marco-willian <marcowcandido@gmail.com>
2023-08-30 15:48:02 -0300

    3mod

--- controlador_de_sons/som.js
+++ controlador_de_sons/som.js
@@ -5,89 +5,103 @@ var matrizSons = [];
 
 console.log(arraySons);
 
-
-for (var j = 0; j < 6; j++) {
-    var coluna = [];
-    for (var i = 0; i < 6; i++) {
-      arraySons.push({
-        linha: i,
-        coluna: j,
-        ativo: false,
-        div: null,
-      });
-      var elemento = document.querySelector(
-        '.som[data-row="' + i + '"][data-col="' + j + '"]'
-      );
-      coluna.push(elemento);
-      elemento.setAttribute("id", i + "-" + j);
-    }
-    matrizSons.push(coluna);
+for (var j = 0; j < 33; j++) {
+  var coluna = [];
+  for (var i = 0; i < 6; i++) {
+    arraySons.push({
+      linha: i,
+      coluna: j,
+      ativo: false,
+      div: null,
+    });
+    var elemento = document.querySelector(
+      '.som[data-row="' + i + '"][data-col="' + j + '"]'
+    );
+    coluna.push(elemento);
+    elemento.setAttribute("id", i + "-" + j);
   }
+  matrizSons.push(coluna);
+}
 
-  // console.log(matrizSons);
+console.log(matrizSons);
 
 var arraysPorColuna = {};
 
-arraySons.forEach(function(elemento){
+arraySons.forEach(function (elemento) {
   var coluna = elemento.coluna;
-  if(!arraysPorColuna[coluna]){
+  if (!arraysPorColuna[coluna]) {
     arraysPorColuna[coluna] = [];
   }
-  arraysPorColuna[coluna].push(elemento)
-})
+  arraysPorColuna[coluna].push(elemento);
+});
 
 // console.log(arraysPorColuna)
 
 matrizSons.forEach(function (linha) {
-    linha.forEach(function (elemento) {
-      elemento.addEventListener("click", function () {
-        var linha = elemento.id.split("-")[0];
-        var coluna = elemento.id.split("-")[1];
-        var itemDoArraySelecionado = arraySons.find((item) => item.linha == linha && item.coluna == coluna);
-        if (itemDoArraySelecionado) {
-          itemDoArraySelecionado.ativo = !itemDoArraySelecionado.ativo;
-          arraysPorColuna[coluna][linha].ativo = itemDoArraySelecionado.ativo;
-          itemDoArraySelecionado.div = itemDoArraySelecionado.ativo ? elemento: null;
-        }
-        // console.log(arraySons);
-        console.log(arraysPorColuna)
-      });
+  linha.forEach(function (elemento) {
+    elemento.addEventListener("click", function () {
+      var linha = elemento.id.split("-")[0];
+      var coluna = elemento.id.split("-")[1];
+      var itemDoArraySelecionado = arraySons.find(
+        (item) => item.linha == linha && item.coluna == coluna
+      );
+      if (itemDoArraySelecionado) {
+        itemDoArraySelecionado.ativo = !itemDoArraySelecionado.ativo;
+        arraysPorColuna[coluna][linha].ativo = itemDoArraySelecionado.ativo;
+        itemDoArraySelecionado.div = itemDoArraySelecionado.ativo
+          ? elemento
+          : null;
+      }
+      // console.log(arraySons);
+      console.log(arraysPorColuna);
     });
   });
-  
-  function reproduzirSons(){
-    var colunaAtual = 0;
-    var linha = 0;
-  
-    function tocarColuna(){
-      if(colunaAtual >= 6){
-        return;
+});
+
+var isPlaying = false;
+
+function reproduzirSons() {
+  var colunaAtual = 0;
+  var linha = 0;
+
+  if(isPlaying){
+    isPlaying = false;
+    return;
+  }
+
+  function tocarColuna() {
+    if (colunaAtual >= 33) {
+      colunaAtual = 0;
+    }
+
+    var sonsAtivosDaColuna = [];
+    for (var linha = 0; linha < 6; linha++) {
+      if (arraysPorColuna[colunaAtual][linha].ativo) {
+        sonsAtivosDaColuna.push(arraysPorColuna[colunaAtual][linha].div);
       }
-  
-      var sonsAtivosDaColuna = [];
-      for(var linha = 0; linha < 6; linha++){
-        if(arraysPorColuna[colunaAtual][linha].ativo){
-          sonsAtivosDaColuna.push(arraysPorColuna[colunaAtual][linha].div);
-        }
-      }
-  
-      sonsAtivosDaColuna.forEach(function(elemento){
-        if(elemento){
+    }
+    sonsAtivosDaColuna.forEach(function (elemento) {
+        if (elemento) {
           var soundkey = elemento.dataset.sound;
           var som = audio[1][soundkey];
-          var quadrado = new Audio(som);
-  
+
+          var quadrado = new Audio(som); 
           quadrado.currentTime = 0;
           quadrado.play();
+          
         }
-      });
-  
-      colunaAtual++;
-  
-      setTimeout(tocarColuna, 500)
+    });
+
+    colunaAtual++;
+
+    if(isPlaying){
+      setTimeout(tocarColuna, 300);
     }
-    tocarColuna();
   }
-  
-  var botaoPlay = document.getElementById("play");
-  botaoPlay.addEventListener('click',reproduzirSons)
+
+  isPlaying = true;
+  tocarColuna();
+}
+
+var botaoPlay = document.getElementById("play");
+botaoPlay.addEventListener("click", reproduzirSons);
