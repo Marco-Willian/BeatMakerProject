f042069ca994747a5b238ea0b534c58e0c3f3911
Marco-Willian <marcowcandido@gmail.com>
2023-10-04 17:16:25 -0300

    web audio api att

--- controlador_de_sons/som.js
+++ controlador_de_sons/som.js
@@ -13,6 +13,7 @@ for (var j = 0; j < 32; j++) {
       coluna: j,
       ativo: false,
       div: null,
+      audioNode: null,
     });
     var elemento = document.querySelector(
       '.som[data-row="' + i + '"][data-col="' + j + '"]'
@@ -58,14 +59,70 @@ matrizSons.forEach(function (linha) {
   });
 });
 
+var botaoTrilha = document.getElementById("trilha");
+botaoTrilha.addEventListener("click", function () {
+
+  var ativo = botaoTrilha.getAttribute("data-ativo") == "true";
+  botaoTrilha.setAttribute("data-ativo", !ativo);
+});
+
+var trilhaSourceNode; // Declarar a variável para o nó de origem da trilha
+
+function reproduzirTrilha() {
+  var trilhaAtiva = botaoTrilha.getAttribute("data-ativo") == "true";
+  if (trilhaAtiva) {
+    var soundKey = botaoTrilha.dataset.sound;
+    var somTrilha = audio[1][soundKey];
+
+    if(trilhaSourceNode && trilhaSourceNode.state === 'started'){
+      trilhaSourceNode.stop();
+      trilhaSourceNode.disconnect();
+    }
+
+    // Criar o nó de origem para a trilha, se ainda não existir
+    
+      trilhaSourceNode = audioContext.createBufferSource();
+      fetch(somTrilha)
+        .then(function (response) {
+          return response.arrayBuffer();
+        })
+        .then(function (arrayBuffer) {
+          audioContext.decodeAudioData(arrayBuffer, function (buffer) {
+            trilhaSourceNode.buffer = buffer;
+            trilhaSourceNode.connect(audioContext.destination);
+            trilhaSourceNode.loop = true; // Configurar o loop infinito
+            trilhaSourceNode.start(0);
+          });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    //  else {
+    //   // Se o nó de origem já existir, basta reiniciar a trilha
+    //   trilhaSourceNode.start(0);
+    // }
+  }
+}
+
 var isPlaying = false;
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 function reproduzirSons() {
   var colunaAtual = 0;
   var linha = 0;
 
-  if(isPlaying){
+  if (isPlaying) {
     isPlaying = false;
+    arraySons.forEach(function (elemento) {
+      if (elemento.audioNode) {
+        elemento.audioNode.stop();
+        elemento.audioNode.disconnect();
+      }
+    });
+    if(trilhaSourceNode){
+      trilhaSourceNode.stop();
+      trilhaSourceNode.disconnect();
+    }
     return;
   }
 
@@ -77,29 +134,53 @@ function reproduzirSons() {
     var sonsAtivosDaColuna = [];
     for (var linha = 0; linha < 6; linha++) {
       if (arraysPorColuna[colunaAtual][linha].ativo) {
-        sonsAtivosDaColuna.push(arraysPorColuna[colunaAtual][linha].div);
+        sonsAtivosDaColuna.push(arraysPorColuna[colunaAtual][linha]);
       }
     }
-    sonsAtivosDaColuna.forEach(function (elemento) {
-        if (elemento) {
-          var soundkey = elemento.dataset.sound;
-          var som = audio[1][soundkey];
 
-          var quadrado = new Audio(som); 
-          quadrado.currentTime = 0;
-          quadrado.play();
-          
+    var promises = sonsAtivosDaColuna.map(function (elemento) {
+      return new Promise(function (resolve, reject) {
+        if (elemento) {
+          var soundKey = elemento.div.dataset.sound;
+          var som = audio[1][soundKey];
+
+          fetch(som)
+            .then(function (response) {
+              return response.arrayBuffer();
+            })
+            .then(function (arrayBuffer) {
+              audioContext.decodeAudioData(arrayBuffer, function (buffer) {
+                var source = audioContext.createBufferSource();
+                source.buffer = buffer;
+                source.connect(audioContext.destination);
+                source.start(0);
+                elemento.audioNode = source;
+                resolve();
+              });
+            })
+            .catch(function (err) {
+              reject(err);
+            });
+        } else {
+          resolve();
         }
+      });
     });
 
-    colunaAtual++;
+    Promise.all(promises)
+      .then(function () {
+        colunaAtual++;
 
-    if(isPlaying){
-      setTimeout(tocarColuna, 150);
-    }
+        if (isPlaying) {
+          setTimeout(tocarColuna, 350);
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
-
   isPlaying = true;
+  reproduzirTrilha();
   tocarColuna();
 }
 
